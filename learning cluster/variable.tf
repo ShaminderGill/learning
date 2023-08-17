ffdb3204f3aa1df30f0a3ee9cc720637d3da6774
ShaminderGill <singhshaminder1322@gmail.com>
2023-08-16 23:26:26 -0400

    new

--- learning cluster/variable.tf
+++ learning cluster/variable.tf
@@ -1,7 +1,7 @@
 variable "azure_resource_name" {
   type = string
   description = "Common name for azure resources"
-  default = "aks_automate_service1"
+  default = "aks_automate_service5"
 }
 
 variable "resource_group_name" {
